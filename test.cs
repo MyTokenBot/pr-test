13353eae2c9d2338f103462cc1d92bd1754fb1fd
MyTokenBot <durieuxthomas+MyTokenBot@gmail.com>
2024-11-08 07:29:22 -0800

    Update test.cs

--- test.cs
+++ test.cs
@@ -13,7 +13,6 @@ public class UsersController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<User>> GetUser(int id)
     {
-        // Vulnerable code
         var user = await _context.Users.FindAsync(id);
         if (user == null)
         {
