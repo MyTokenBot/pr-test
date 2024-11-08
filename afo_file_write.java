2aec99a65232c235e2b4878a5e72e801a80528d5
MyTokenBot <durieuxthomas+MyTokenBot@gmail.com>
2024-11-08 06:37:33 -0800

    Create afo_file_write.java

---
+++ afo_file_write.java
@@ -0,0 +1,14 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileWriteExample {
+    public static void main(String[] args) {
+        // Vulnerable code
+        String filename = args[0];
+        try (FileOutputStream fos = new FileOutputStream(filename)) {
+            fos.write("Example content".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
