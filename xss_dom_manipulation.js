d240826074f42348daa07bbff4c56fe8353d2fd1
MyTokenBot <durieuxthomas+MyTokenBot@gmail.com>
2024-11-08 06:38:28 -0800

    Create xss_dom_manipulation.js

---
+++ xss_dom_manipulation.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/echo', (req, res) => {
+    // Vulnerable code
+    const userInput = req.query.input;
+    res.send(`<div>You said: ${userInput}</div>`);
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
