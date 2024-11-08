94402c55bdc1aaca3f36a36018a77dc475256e51
MyTokenBot <durieuxthomas+MyTokenBot@gmail.com>
2024-11-08 06:47:51 -0800

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class UsersController : ControllerBase
+{
+    private readonly UserContext _context;
+
+    public UsersController(UserContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(int id)
+    {
+        // Vulnerable code
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
+}
