83ef3b60a0e3e0729c4db7934f5925888495b32a
MyTokenBot <durieuxthomas+MyTokenBot@gmail.com>
2024-11-08 07:31:46 -0800

    Update ssrf_url_fetch.py

--- ssrf_url_fetch.py
+++ ssrf_url_fetch.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/fetch_url')
 def fetch_url():
-    # Vulnerable code
     url = request.args.get('url')
     response = requests.get(url)
     return response.text
