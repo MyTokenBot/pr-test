6dceffbd0be88a958f9c0f2f814496f2450bf8cf
MyTokenBot <durieuxthomas+MyTokenBot@gmail.com>
2024-11-08 06:38:01 -0800

    Create sqli_statement_building.java

---
+++ sqli_statement_building.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SQLInjectionExample {
+    public static void main(String[] args) {
+        // Vulnerable code
+        String userId = args[0];
+        String sql = "SELECT * FROM users WHERE id = '" + userId + "'";
+        try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb");
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                System.out.println(rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
