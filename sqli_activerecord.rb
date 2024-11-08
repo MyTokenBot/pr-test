acda6cfd89b6def6b8d0a4a5b3025fa460818dff
MyTokenBot <durieuxthomas+MyTokenBot@gmail.com>
2024-11-08 07:28:07 -0800

    Update sqli_activerecord.rb

--- sqli_activerecord.rb
+++ sqli_activerecord.rb
@@ -1,12 +1,5 @@
-# config/routes.rb
-Rails.application.routes.draw do
-  get 'users', to: 'users#index'
-end
-
-# app/controllers/users_controller.rb
 class UsersController < ApplicationController
   def index
-    # Vulnerable code
     @users = User.where("name = '#{params[:name]}'")
     render json: @users
   end
